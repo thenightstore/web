e41ece6cacbc2af913591a940f7c9d7d066528bc
thenightstore <164535613+thenightstore@users.noreply.github.com>
2024-03-23 12:46:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ overlay.addEventListener("click", ()=>{
     navlinks.style.left = "-150%";
     overlay.style.display = "none"
 })
+
+const accordionItem = document.querySelectorAll(".accordion-item");
+
+accordionItem.forEach((el) =>
+
+    el.addEventListener("click", () => {
+        if (el.classList.contains("faq-active")) {
+            el.classList.remove("faq-active");
+        } else {
+            accordionItem.forEach((el) => el.classList.remove("faq-active"));
+            el.classList.add("faq-active");
+        }
+    })
+);
