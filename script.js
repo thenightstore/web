7b0ff606e57c22a1c158cb86f9cb0a95eb3b12e6
thenightstore <164535613+thenightstore@users.noreply.github.com>
2024-03-24 16:48:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menubtn = document.querySelector(".menu-btn")
 const overlay = document.querySelector(".overlay")
 const navlinks = document.querySelector(".nav-links")
+const navlink = document.querySelectorAll(".nav-link a")
 menubtn.addEventListener("click", ()=>{
     navlinks.style.left = "0%";
     overlay.style.display = "block"
@@ -9,6 +10,29 @@ overlay.addEventListener("click", ()=>{
     navlinks.style.left = "-150%";
     overlay.style.display = "none"
 })
+for (let i = 0; i < navlink.length; i++) {
+    navlink[0].addEventListener("click", () => {
+        navlinks.style.left = "-150%";
+        overlay.style.display = "none"
+    })
+    navlink[1].addEventListener("click", () => {
+        navlinks.style.left = "-150%";
+        overlay.style.display = "none"
+    })
+    navlink[2].addEventListener("click", () => {
+        navlinks.style.left = "-150%";
+        overlay.style.display = "none"
+    })
+    navlink[3].addEventListener("click", () => {
+        navlinks.style.left = "-150%";
+        overlay.style.display = "none"
+    })
+    navlink[4].addEventListener("click", () => {
+        navlinks.style.left = "-150%";
+        overlay.style.display = "none"
+    })
+    
+}
 
 const accordionItem = document.querySelectorAll(".accordion-item");
 
